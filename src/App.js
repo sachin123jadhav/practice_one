72640e3f80f3b9aad00c864fcde67461ac25c7f0
Sachin <sachin123jadhav@gmail.com>
2023-08-21 18:16:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Restaurant from './component/Basic/Restaurant'
+
+const App = () => {
+  return (
+    <div>
+
+<Restaurant/>
+    </div>
+    
+  )
+}
+
+export default App
